a78ae812b64d5785251887c79d5b52a098b48457
Sravya GLS <lagrandhi@paypal.com>
2021-04-26 07:48:46 +0530

    cross origin

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_KEY = 'afd5f2e93b50e0d6e0fc55f1fd057e7e'
-const BASE_URL =  `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=metric`
+const BASE_URL =  `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}&units=metric`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER'
 export const FETCH_DATA_ERROR = 'FETCH_DATA_ERROR'
